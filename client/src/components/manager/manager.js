1c903f3b8745401d732404ba59758996845701d0
BlackieStars <diviprakashpc@gmail.com>
2022-04-16 12:55:56 +0530

    Added next-prev functionality,Now going to give functionality to each card

--- client/src/components/manager/manager.js
+++ client/src/components/manager/manager.js
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import "./manager.css";
 import Navbar from "../Navbar/navbar";
 import ManagerCard from "../ManagerCard/Card";
@@ -8,10 +8,13 @@ import { currentUser, setsLoginUser } from "../../App";
 const Manager = () => {
   const user = useContext(currentUser);
   const setLoginUser = useContext(setsLoginUser);
- 
   const [cardList, setCardList] = useState(user.list);
+  const [page, setPage] = useState(0);
   // console.log("Initial List of user",user.list,"cardList intial",cardList.length);
   // console.log("user at manager", user);
+  //90-99(range of index of element)->9(page)
+  //100-109->10
+  //180-189->18
   return (
     <>
       <div id="manager">
@@ -21,13 +24,21 @@ const Manager = () => {
         <section id="manager-section2">
           <div id="manager-cards">
             <div className="row manager-card-row">
-              {cardList.splice(0).reverse().map((key,index) => {
-                return (
-                  <div className="col-lg-15">
-                    <ManagerCard email={key.email} website={key.website} password={key.password} />
-                  </div>
-                );
-              })}
+              {cardList
+                .slice(0)
+                .reverse()
+                .slice(page * 10, (page + 1) * 10)
+                .map((key, index) => {
+                  return (
+                    <div className="col-lg-15">
+                      <ManagerCard
+                        email={key.email}
+                        website={key.website}
+                        password={key.password}
+                      />
+                    </div>
+                  );
+                })}
             </div>
           </div>
           <div id="manager-add-card">
@@ -35,8 +46,29 @@ const Manager = () => {
           </div>
         </section>
         <section id="manager-section3">
-          <button className="btn btn-primary">Prev</button>
-          <button className="btn btn-primary">Next</button>
+          <button
+            className="btn btn-primary"
+            onClick={() => {
+              if (page - 1 < 0) {
+                alert("No page exist below 0,Sorry Mate");
+              } else setPage(page - 1);
+            }}
+          >
+            Prev
+          </button>
+          <div id="page-no">{page}</div>
+          <button
+            className="btn btn-primary"
+            onClick={() => {
+              if (cardList.length > (page + 1) * 10) {
+                setPage(page + 1);
+              } else {
+                alert("Sorry mate,nothing ahead");
+              }
+            }}
+          >
+            Next
+          </button>
         </section>
       </div>
     </>
